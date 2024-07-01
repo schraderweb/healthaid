4679df63737377bf4c80c72f95947d6cb75451ee
Naaman kiptoo <kiptoon457@gmail.com>
2024-07-02 02:31:28 +0300

    implemented necessary changes

--- js/app.js
+++ js/app.js
@@ -5,23 +5,20 @@ const q2BtnNo = document.querySelector(".q2-btn-no");
 const loading3 = document.getElementById("loading3");
 const loading2 = document.getElementById("loading2");
 const loading1 = document.getElementById("loading1");
+
 const q2Btn = document.querySelector("#q2");
 const q1Btn = document.querySelector("#q1");
-const timerElement = document.getElementById('timer');
-let btns = document.querySelectorAll("div.button")
+let choiceUnder = true;
+let choiceMedical = true;
 
-
-let choiceUnder = null;
-let choiceMedical = null;
-
-const timeLimitInMinutes = 4;
-let timeLimitInSeconds = timeLimitInMinutes * 60;
-let timerInterval;
+var timeLimitInMinutes = 4;
+var timeLimitInSeconds = timeLimitInMinutes * 60;
+var timerElement = document.getElementById('timer');
 
 function startTimer() {
   timeLimitInSeconds--;
-  let minutes = Math.floor(timeLimitInSeconds / 60);
-  let seconds = timeLimitInSeconds % 60;
+  var minutes = Math.floor(timeLimitInSeconds / 60);
+  var seconds = timeLimitInSeconds % 60;
 
   if (timeLimitInSeconds < 0) {
     timerElement.textContent = '00:00';
@@ -29,103 +26,73 @@ function startTimer() {
     return;
   }
 
-  timerElement.textContent = `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+
+  timerElement.textContent = minutes + ':' + seconds;
 }
 
-function showText4() {
-
-  
-
-  if (choiceUnder === false || choiceMedical === true) {
-    document.getElementById("qualify").style.display = "block";
-    sh.initialize(matchingConfiguration, "qsWidgetContainer");
-  } else if(choiceUnder && choiceMedical === false) {
-     document.getElementById("dontqualify").style.display = "block"
-  } 
-  else {
-    document.getElementById("qualify").style.display = "block"
-
+const showText4 = () => {
+    if(choiceUnder == true  ) {
+        var timerInterval = setInterval(startTimer, 1000);
+        document.getElementById("qualify").style.display = "block";
+    }else {
+        document.getElementById("dontqualify").style.display = "block";
+    }
 }
+const showText3 = () => {
+    loading3.style.display = "block";
+    setTimeout(() => {
+        console.log('Foo bar1');
+        loading3.style.display = "none";
+        showText4();
+      }, 1000)
 }
-
-function showText3() {
-  loading3.style.display = "block";
-  setTimeout(() => {
-    loading3.style.display = "none";
-    showText4();
-  }, 1000);
+const showText2 = () => {
+    loading2.style.display = "block";
+    setTimeout(() => {
+        console.log('Foo bar1');
+        loading2.style.display = "none";
+        showText3();
+      }, 1000)
 }
-
-function showText2() {
-  loading2.style.display = "block";
-  setTimeout(() => {
-    loading2.style.display = "none";
-    showText3();
-  }, 1000);
-}
-
-function showText1() {
-  loading1.style.display = "block";
-  setTimeout(() => {
-    loading1.style.display = "none";
-    showText2();
-  }, 1000);
+const showText1 = () => {
+    loading1.style.display = "block";
+    setTimeout(() => {
+        console.log('Foo bar');
+        loading1.style.display = "none";
+        showText2();
+      }, 1000)
 }
 
 q1BtnYes.addEventListener("click", () => {
-  q1Btn.style.display = "none";
-  q2Btn.style.display = "block";
-  choiceUnder = true;
+    q1Btn.style.display = "none";
+    q2Btn.style.display = "block";
+    q1Btn.classList.add("fade-in");
+    q2Btn.classList.add("fade-out");
+    choiceUnder = true;
 });
-
 q1BtnNo.addEventListener("click", () => {
-  hideAllBtns()
-  q1Btn.style.display = "none";
-  q2Btn.style.display = "block";
-
-  choiceUnder = false;
-  setTimeout(() => {
-    loading1.style.display = "none";
-    loading2.style.display = "block";
-    setTimeout(() => {
-      loading2.style.display = "none";
-      loading3.style.display = "block";
-      setTimeout(() => {
-        loading3.style.display = "none";
-        showText4()
-      }, 1000);
-    }, 1000);
-  }, 0);
-  
+    q1Btn.style.display = "none";
+    q2Btn.style.display = "none";
+    q2Btn.classList.add("fade-in");
+    loading1.classList.add("fade-out");
+    choiceUnder = false;
+    showText1();
 });
 
 q2BtnYes.addEventListener("click", () => {
-  q2Btn.style.display = "none";
-  choiceMedical = true;
-  showText1();
+    q2Btn.style.display = "none";
+    choiceMedical = false;
+    choiceUnder = false
+    showText1();
 });
-
 q2BtnNo.addEventListener("click", () => {
-  q2Btn.style.display = "none";
-  choiceMedical = false;
-  loading1.style.display = "block";
-  setTimeout(() => {
-    loading1.style.display = "none";
-    loading2.style.display = "block";
-    setTimeout(() => {
-      loading2.style.display = "none";
-      loading3.style.display = "block";
-      setTimeout(() => {
-        loading3.style.display = "none";
-        document.getElementById("dontqualify").style.display = "block";
-        timerInterval = setInterval(startTimer, 1000);
-      }, 1000);
-    }, 1000);
-  }, 1000);
+    q2Btn.style.display = "none";
+    choiceMedical = false;
+    showText1();
 });
-
-
-function hideAllBtns() {
-  document.querySelectorAll('.button').forEach(btn => btn.style.display="none")
-
-}
