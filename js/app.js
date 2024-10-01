d564c372fb2acbf5911a4b60d13822f5833543d1
Stonehedge123 <156220065+Stonehedge123@users.noreply.github.com>
2024-10-01 13:57:17 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -94,6 +94,7 @@ q1BtnNo.addEventListener("click", () => {
 q2BtnYes.addEventListener("click", () => {
     q2Btn.style.display = "none";
     choiceMedical = true;
+  showText2();
     });
 q2BtnNo.addEventListener("click", () => {
     q2Btn.style.display = "none";
