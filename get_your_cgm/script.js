8dd7ca45cbac16acee9b95a062589d90e2d51ef0
Stonehedge123 <156220065+Stonehedge123@users.noreply.github.com>
2026-03-06 11:13:39 -0500

    Refactor question handling and qualification logic

--- get_your_cgm/script.js
+++ get_your_cgm/script.js
@@ -98,7 +98,7 @@ medicare Benefits.
                             
                             
                             <h1>CALL NOW TO CLAIM YOUR CGM </h1>
-                            <a href="tel:18337230131" class="call-button" id="callNowBtn">
+                            <a href="tel:18445261473" class="call-button" id="callNowBtn">
                                 <i class="fas fa-phone"></i> 1-833-723-0131   
                             </a>
                             <h4>Pre-Order Hold Expires in: <span style="color: #ff0000;">00:00</span></h4>
@@ -161,5 +161,6 @@ medicare Benefits.
                     display.textContent = "00:00";
                 }
             }, 1000);
+
 
         }
