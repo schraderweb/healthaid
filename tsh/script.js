c6e4d536db233530e5fc43c9137d11cf5a2f697c
Stonehedge123 <156220065+Stonehedge123@users.noreply.github.com>
2026-03-11 16:31:49 -0400

    Refactor question navigation and qualification logic

--- tsh/script.js
+++ tsh/script.js
@@ -98,7 +98,7 @@ medicare Benefits.
                             
                             <h1>CALL NOW TO CLAIM YOUR CGM </h1>
                             <a href="tel:18445261473" class="call-button" id="callNowBtn">
-                                <i class="fas fa-phone"></i> 1-833-723-0131   
+                                <i class="fas fa-phone"></i> 1-844-526-1473
                             </a>
                             <h4>Pre-Order Hold Expires in: <span style="color: #ff0000;">00:00</span></h4>
                             <p><i class="fas fa-exclamation-triangle"></i> LOW STOCK ALERT</p>
@@ -162,4 +162,5 @@ medicare Benefits.
             }, 1000);
 
         }
+
 
